9df682b8bc2f11268aebadb58af3996b70523420
Ian Roller <ianroller4@gmail.com>
2025-10-15 14:46:46 -0500

    Damaged Ship

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -8,19 +8,40 @@ public class Health : MonoBehaviour
     private float currentHP;
     public float MAX_HP = 3f;
 
+    // --- Damage Sprite ---
+    public Sprite damaged;
+    private Sprite original;
+    private SpriteRenderer spriteRenderer;
+
     // --- Switches ---
     public bool DEBUG_MODE = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = transform.Find("Art").gameObject.GetComponent<SpriteRenderer>();
+        original = spriteRenderer.sprite;
         currentHP = MAX_HP;
+    }
+
+    private IEnumerator DamageFlicker()
+    {
+        if (damaged != null)
+        {
+            spriteRenderer.sprite = damaged;
+            yield return new WaitForSeconds(0.1f);
+            spriteRenderer.sprite = original;
+        }
     }
 
     public bool TakeDamage(float damage)
     {
         bool result = false;
         currentHP -= damage;
+        if (currentHP > 0 && damaged != null)
+        {
+            StartCoroutine(DamageFlicker());
+        }
         if (DEBUG_MODE)
         {
             Debug.Log("Hit!");
